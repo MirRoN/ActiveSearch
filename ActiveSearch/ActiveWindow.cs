c9d91c1773f71c4230db06cf09280a49912df672
MirRoN <miron.mvi@gmail.com>
2014-10-24 15:36:46 +0300

    Initial commit
    Initial commit

---
+++ ActiveSearch/ActiveWindow.cs
@@ -0,0 +1,114 @@
+﻿namespace ActiveSearch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+    using System.IO;
+    using System.Diagnostics;
+
+    public partial class ActiveWindow : Form
+    {
+        private const int maxDiff = 300;
+
+        internal string advFramePath = string.Empty;
+        internal string videoFilePath = string.Empty;
+        public ActiveWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectAdvFrame_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fDialog = new OpenFileDialog();
+            fDialog.Title = "Open advertisment frame";
+            fDialog.Filter = "Image files|*.jpg;*.jpeg;*.bmp;*.gif;*.png;*.tif;*.tiff";
+            fDialog.InitialDirectory = @"C:\";
+            fDialog.AddExtension = true;
+            if (fDialog.ShowDialog() == DialogResult.OK)
+            {
+                advFramePath = fDialog.FileName.ToString().Replace('\\', '/');
+            }
+        }
+
+        private void SelectVideo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fDialog = new OpenFileDialog();
+            fDialog.Title = "Open advertisment frame";
+            fDialog.Filter = "MP4 files|*.mp4;*.mpeg4";
+            fDialog.InitialDirectory = @"C:\";
+            fDialog.AddExtension = true;
+            if (fDialog.ShowDialog() == DialogResult.OK)
+            {
+                videoFilePath = fDialog.FileName.ToString().Replace('\\', '/');
+            }
+        }
+        private void SplitFrames_Click(object sender, EventArgs e)
+        {
+            if (videoFilePath != string.Empty)
+            {
+                Utils.ClearFolder("..\\Debug\\SplittedFiles");
+                string commandLine = "-i " + videoFilePath + " -sameq SplittedFiles/frame_num%05d.jpg";
+                Process.Start("ffmpeg_32.exe", commandLine);
+            }
+            else
+            {
+                MessageBox.Show("Please select video file!");
+            }
+        }
+
+        private void BeginSearch_Click(object sender, EventArgs e)
+        {
+            if (advFramePath != string.Empty)
+            {
+                Color[,] advFrame = Utils.PortionOfImageToColorArray(
+                    new Bitmap(advFramePath), 135, 285, 165, 315);
+                List<Color[,]> videoFrames =
+                    Utils.GetVideoFramesAsColorMatrix("..\\Debug\\SplittedFiles");
+
+                List<double> matchFrames = new List<double>();
+                for (int i = 0; i < videoFrames.Count; i++)
+                {
+                    if (i % 3 != 0)
+                    {
+                        continue;
+                    }
+                    if (Utils.CalcDifference(advFrame, videoFrames[i]) <= maxDiff)
+                    {
+                        matchFrames.Add(i / 1500.0);
+                    }
+                }
+                if (matchFrames.Count > 0)
+                {
+                    MessageBox.Show(matchFrames[0].ToString());
+                }
+                else
+                {
+                    MessageBox.Show("No matching frame or no video loaded!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an advertisement frame!");
+            }
+
+        }
+
+        private void UnloadVideoFrames_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Utils.ClearFolder("..\\Debug\\SplittedFiles");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Problem occurred while deleting files! Please try again!");
+            }
+        }
+    }
+}
---
+++ ActiveSearch/Utils.cs
@@ -0,0 +1,129 @@
+﻿namespace ActiveSearch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+
+    public static class Utils
+    {
+        public static long CalcDifference(Color[,] imageA, Color[,] imageB)
+        {
+            long difference = 0;
+            for (int i = 0; i < imageA.GetLength(0); i++)
+            {
+                for (int j = 0; j < imageA.GetLength(1); j++)
+                {
+                    difference += Math.Abs(imageA[i, j].R - imageB[i, j].R)
+                        + Math.Abs(imageA[i, j].G - imageB[i, j].G)
+                        + Math.Abs(imageA[i, j].B - imageB[i, j].B);
+                }
+            }
+            return difference;
+        }
+
+        public static Color[,] PortionOfImageToColorArray(System.Drawing.Bitmap imageIn, int xB, int yB, int xE, int yE)
+        {
+            Color[,] colorMatrix = new Color[xE - xB, yE - yB];
+            using (imageIn)
+            {
+                for (int i = xB; i < xE; i++)
+                {
+                    for (int j = yB; j < yE; j++)
+                    {
+                        colorMatrix[i - xB, j - yB] = imageIn.GetPixel(i, j);
+                    }
+                }
+            }
+            return colorMatrix;
+        }
+
+        internal static List<long> CalcAllDiffInFolder(string folderPath)
+        {
+            string[] paths = Directory.GetFiles(folderPath, "*.jpg",
+                                     SearchOption.TopDirectoryOnly);
+            List<long> diff = new List<long>();
+
+            try
+            {
+                for (int i = 0; i < paths.Length - 1; i++)
+                {
+                    Bitmap bitmapOne = (Bitmap)Image.FromFile(paths[i], true);
+                    Bitmap bitmapTwo = (Bitmap)Image.FromFile(paths[i + 1], true);
+                    long differrence = Utils.CalcDifference(Utils.PortionOfImageToColorArray(bitmapOne, 130, 280, 170, 320),
+                    Utils.PortionOfImageToColorArray(bitmapTwo, 130, 280, 170, 320));
+                    diff.Add(differrence);
+                }
+
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("There was an error opening the bitmap. Please check the path.");
+            }
+            return diff;
+        }
+
+        internal static void ExportList(List<double> listOfDifferences, string outputFilePath)
+        {
+            StreamWriter wr = new StreamWriter(outputFilePath);
+            using (wr)
+            {
+                foreach (var item in listOfDifferences)
+                {
+                    wr.WriteLine("Frame found approximately at: {0:#,##0}", item);
+                }
+            }
+        }
+
+        internal static List<long> GetScenesPattern(List<long> list, long threshold)
+        {
+            List<long> intervals = new List<long>();
+            long count = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] < threshold)
+                {
+                    count++;
+                }
+                else
+                {
+                    intervals.Add(count);
+                    count = 0;
+                }
+            }
+            intervals.Add(count);
+            return intervals;
+        }
+
+        internal static List<Color[,]> GetVideoFramesAsColorMatrix(string folderPath)
+        {
+            string[] paths = Directory.GetFiles(folderPath, "*.jpg",
+                                     SearchOption.TopDirectoryOnly);
+            List<Color[,]> frames = new List<Color[,]>();
+            for (int i = 0; i < paths.Length; i++)
+            {
+                frames.Add(Utils.PortionOfImageToColorArray(
+                    new Bitmap(paths[i]), 135, 285, 165, 315));
+            }
+            return frames;
+        }
+
+        internal static void ClearFolder(string folderName)
+        {
+            DirectoryInfo dir = new DirectoryInfo(folderName);
+
+            foreach (FileInfo fi in dir.GetFiles())
+            {
+                fi.IsReadOnly = false;
+                fi.Delete();
+            }
+
+            foreach (DirectoryInfo di in dir.GetDirectories())
+            {
+                ClearFolder(di.FullName);
+                di.Delete();
+            }
+        }
+    }
+}
